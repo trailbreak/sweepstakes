7d0cd962076e67beea23f456ca74413b0be21851
Rafael Robayna <rrobayna@gmail.com>
2020-04-07 12:46:56 -0400

    Updated selector to integrate a deterministic RNG

--- selector.js
+++ selector.js
@@ -1,5 +1,6 @@
 const lineReader = require('line-reader'),
-      Promise = require('bluebird');
+      Promise = require('bluebird'),
+	  Prando = require('prando');
 
 let likes = []
 let follows = []
@@ -11,25 +12,23 @@ var eachLine = Promise.promisify(lineReader.eachLine);
 eachLine('./entries/likes', function(line) {
 	appendEntry(line, 1)
 }).then(function() {
-	pickWinner('likes');
-});
-
-eachLine('./entries/follows', function(line) {
-	appendEntry(line, 1)
-}).then(function() {
-	pickWinner('follows');
-});
-
-eachLine('./entries/retweets', function(line) {
-	appendEntry(line, 1)
-}).then(function() {
-	pickWinner('retweets');
-});
-
-eachLine('./entries/delegators', function(line) {
-	appendEntry(line, 3)
-}).then(function() {
-	pickWinner('delegators');
+	console.log("Loaded likes")
+ 	eachLine('./entries/follows', function(line) {
+ 		appendEntry(line, 1)
+ 	}).then(function() {
+		console.log("Loaded follows")
+ 		eachLine('./entries/retweets', function(line) {
+			appendEntry(line, 1)
+		}).then(function() {
+			console.log("Loaded retweets")
+			eachLine('./entries/delegators', function(line) {
+				appendEntry(line, 3)
+			}).then(function() {
+				console.log("Loaded delegators")
+				pickWinner();
+			});
+		});
+	});
 });
 
 let entries = []
@@ -43,15 +42,16 @@ function appendEntry(entry, weight) {
 }
 
 var loadCount = 0
-function pickWinner(fileName) {
-	console.log("Loaded " + fileName)
-	loadCount++
-	if (loadCount < 4) {
-		return
-	}
+function pickWinner() {
+	d = new Date()
+	ts = d.getTime()
+	console.log("Date:", d)
+	console.log("Timestamp:", ts)
 
+	let rng = new Prando(ts);
+	let winningIdx = rng.nextInt(0, entries.length);
 	console.log(entries)
 
-	var winner = entries[Math.floor(Math.random()*entries.length)];
+	var winner = entries[winningIdx];
 	console.log("The winner is: " + winner)
 }
